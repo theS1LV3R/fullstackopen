27207c491df17abd8fe9346f83cfca3d9c6549cd
theS1LV3R <me@s1lv3r.codes>
2022-09-04 16:46:14 +0200

    Course information step7 (2.2)

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,10 +1,12 @@
 import Content from "./Content";
 import Header from "./Header";
+import Total from "./Total";
 
 const Course = ({ course }) => (
   <div>
     <Header course={course} />
     <Content course={course} />
+    <Total course={course}></Total>
   </div>
 );
 
