886756bf23dd12e7aa7e621126ccb38666017a84
theS1LV3R <me@s1lv3r.codes>
2022-09-04 17:11:30 +0200

    Course information step9 (2.4)
    Also fix a couple small bugs and notifications in the console

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,15 +1,33 @@
 import Course from "./components/Course";
 
 export default function App() {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      { name: "Fundamentals of React", exercises: 10, id: 1 },
-      { name: "Using props to pass data", exercises: 7, id: 2 },
-      { name: "State of a component", exercises: 14, id: 3 },
-    ],
-  };
+  const courses = [
+    {
+      id: 1,
+      name: "Half Stack application development",
+      parts: [
+        { name: "Fundamentals of React", exercises: 10, id: 1 },
+        { name: "Using props to pass data", exercises: 7, id: 2 },
+        { name: "State of a component", exercises: 14, id: 3 },
+        { name: "Redux", exercises: 11, id: 4 },
+      ],
+    },
+    {
+      id: 2,
+      name: "Node.js",
+      parts: [
+        { name: "Routing", exercises: 3, id: 1 },
+        { name: "Middleware", exercises: 7, id: 2 },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return (
+    <>
+      <h1>Fullstackopen curriculum</h1>
+      {courses.map((course) => (
+        <Course course={course} key={course.id} />
+      ))}
+    </>
+  );
 }
--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -1,20 +1,13 @@
 import Part from "./Part";
 
-export default function Content(props) {
+const Content = ({ course }) => {
   return (
     <>
-      <Part
-        part={props.course.parts[0].name}
-        exercises={props.course.parts[0].exercises}
-      />
-      <Part
-        part={props.course.parts[1].name}
-        exercises={props.course.parts[1].exercises}
-      />
-      <Part
-        part={props.course.parts[2].name}
-        exercises={props.course.parts[2].exercises}
-      />
+      {course.parts.map((part) => (
+        <Part part={part.name} exercises={part.exercises} key={part.id} />
+      ))}
     </>
   );
-}
+};
+
+export default Content;
